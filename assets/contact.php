1e4f51b42d97c813352401b67fae87a871d9687c
Pisces-24 <piceee2021@gmail.com>
2024-10-12 08:09:11 +0530

    Add files via upload

---
+++ assets/contact.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+$_SESSION["username"] = $_POST["name"];
+
+if(isset($_POST) && count($_POST) > 0){	
+    $response = 1;
+	$to_email = 'mrithyunjayan.2018@gmail.com';
+$subject = "Path Identifier Command Contact Form";
+$message = "Name: ".$_POST['name']."\r\n";
+$message .= "Phone: ".$_REQUEST['phone']."\r\n";
+$message .= "Email: ".$_POST['email']."\r\n";
+$message .= "Company: ".$_REQUEST['company']."\r\n";
+$message .= "Subject: ".$_REQUEST['subject']."\r\n";
+$message .= "Message: ".$_REQUEST['message']."\r\n";
+
+$headers = "From:support@gs2pic.co.in";
+mail($to_email,$subject,$message,$headers);
+}
+?>
